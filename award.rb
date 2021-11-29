368a5a6bec8c75cd9dd62bdacfd44b3a21fda6c9
Sam Barrientos <stbarrientos@gmail.com>
2021-11-29 13:34:01 -0500

    refactored nested conditionals into methods, replaced Award struct with class and helpers

--- award.rb
+++ award.rb
@@ -1,1 +1,38 @@
-Award = Struct.new(:name, :expires_in, :quality)
+# Award = Struct.new(:name, :expires_in, :quality)
+
+class Award
+  attr_accessor :name, :expires_in, :quality
+
+  def initialize(name, expires_in, quality)
+    @name = name
+    @expires_in = expires_in
+    @quality = quality
+  end
+
+
+  # Increment / decrement the value of quality with a clamp of 0-50
+  # This is the recommended method for changing quality, unless it is being set absolutely
+  def change_quality_by(new_quality)
+    @quality += new_quality
+    @quality = 50 if @quality > 50
+    @quality = 0 if @quality < 0
+    @quality
+  end
+
+  ### Helpers ###
+  def expired?
+    @expires_in <= 0
+  end
+
+  # def expires_today?
+  #   @expires_in == 0
+  # end
+
+  def decrement_expiration
+    @expires_in -= 1
+  end
+
+  def maximum_quality_reached
+    @quality >= 50
+  end
+end
--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,109 @@
 require 'award'
 
+# Plans that increase quality as they age
+INCREASE_AS_AGE_PLANS = ['Blue First', 'Blue Compare'].freeze
+
+# Plans that increase quality after they expire
+INCREASE_AFTER_EXPIRATION_PLANS = ['Blue First'].freeze
+
+# Plans that lose all quality after they expire
+ZERO_AFTER_EXPIRATION_PLANS = ['Blue Compare'].freeze
+
+# Plans that increase quality variably as they age
+VARIABLE_INCREASE_PLANS = ['Blue Compare'].freeze
+
+# Plans that have a static quality of 80
+STATIC_QUALITY_PLANS = ['Blue Distinction Plus'].freeze
+
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
+    # Handle static plans
+    if STATIC_QUALITY_PLANS.include?(award.name)
+      award.quality = 80
+      next
+    end
+
+    # Handle expired awards seperately from aging awards
+    if award.expired?
+      handle_expiration(award)
+    # elsif award.expires_today?
+    #   handle_expires_today(award)
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
+      handle_aging(award)
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
+
+    # Age the award
+    award.decrement_expiration
   end
+end
+
+private
+
+# def handle_expires_today(award)
+#   # Handle plans that decrease value as they age
+#   unless INCREASE_AS_AGE_PLANS.include?(award.name)
+
+#     # These planse decrease quality as they age
+#     award.change_quality_by -2
+#     return
+#   end
+
+#   # Quality cannot exceed 50
+#   return if award.maximum_quality_reached
+
+#   # Handle non-variable increase plans
+#   unless VARIABLE_INCREASE_PLANS.include?(award.name)
+#     award.change_quality_by 2
+#     return
+#   end
+
+#   # Variable plans zero out on the days they expire
+#   award.quality = 0 if award.expires_today?
+# end
+
+def handle_aging(award)
+  # Handle plans that decrease value as they age
+  unless INCREASE_AS_AGE_PLANS.include?(award.name)
+
+    # These planse decrease quality as they age
+    award.change_quality_by -1
+    return
+  end
+
+  # Quality cannot exceed 50
+  return if award.maximum_quality_reached
+
+  # Handle non-variable increase plans
+  unless VARIABLE_INCREASE_PLANS.include?(award.name)
+    award.change_quality_by 1
+    return
+  end
+
+  # Variable plans increase quality by different rates depending on age
+  if award.expires_in < 6
+    # Variable Increase plans increase in quality by 3 if it expires in less than 6 days
+    award.change_quality_by 3
+  elsif award.expires_in < 11
+    # Variable Increase plans increase in quality by 2 if it expires in less than 11 days
+    award.change_quality_by 2
+  else
+    # Variable Increase plans increase in quality by one until they expire
+    award.change_quality_by 1
+  end
+end
+
+def handle_expiration(award)
+  # Handle plans that increase quality after expiration
+  if INCREASE_AFTER_EXPIRATION_PLANS.include?(award.name)
+    award.change_quality_by 2
+    return
+  end
+
+  # Blue Compare plans lose all quality when they expire
+  if award.name == 'Blue Compare'
+    award.quality = 0
+    return
+  end
+
+  award.change_quality_by -2
 end
